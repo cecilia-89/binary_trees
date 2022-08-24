0f390b470302cca4d83c7a5dcbad6dd7401c784e
cecilia-89 <catabong89@gmail.com>
2022-08-25 00:25:37 +0100

    corrections

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,17 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes- returns size of tree
- * @tree: root node
- * Return: height of tree
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: the node to check
+ * Return: 0 on success else 1
  */
 
-size_t binary_tree_nodes(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
 
-	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node != NULL && node->right == NULL && node->left == NULL)
+		return (1);
+
+	return (0);
+
 }
 
 /**
@@ -22,9 +24,14 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree == NULL || binary_tree_nodes(tree->left)
-		!= binary_tree_nodes(tree->right))
+	int left, right;
+	if (tree == NULL)
 		return (0);
 
-	return (1);
+	left = binary_tree_is_leaf(tree->left);
+	right = binary_tree_is_leaf(tree->left);
+
+	if (left == 1 && right == 1)
+		return (1);
+	return (0);
 }
---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes- returns size of tree
+ * @tree: root node
+ * Return: height of tree
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+}
+
+/**
+ * binary_tree_is_perfect- returns size of tree
+ * @tree: root node
+ * Return: 0 if tree is complete
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL || binary_tree_nodes(tree->left)
+		!= binary_tree_nodes(tree->right))
+		return (0);
+
+	return (1);
+}
