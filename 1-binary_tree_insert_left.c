f8a3fbd2b9a161102ea97bb38e29596004820fc6
cecilia-89 <catabong89@gmail.com>
2022-08-25 10:42:11 +0100

    corrections

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,37 +1,33 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - inserts node at leftmost side
+ * binary_tree_insert_right - inserts node at leftmost side
  * @parent: parent of the node
  * @value: interger value
  * Return: node pointer on success
  */
 
-
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-
 	binary_tree_t *node;
 
-	node = malloc(sizeof(binary_tree_t));
-	if (node == NULL || parent == NULL)
-		return (NULL);
-	node->right = NULL;
-	node->n = value;
-	node->parent = parent;
+	if (parent == NULL)
+		return (0);
+
+	node = binary_tree_node(parent, value);
+	if (node == NULL)
+		return (0);
 
 	if (parent->left != NULL)
 	{
-		node->left = parent->left;
+		node->left = parent->right;
 		parent->left = node;
 		node->left->parent = node;
 	}
 
 	else
-	{
-		node->left = NULL;
 		parent->left = node;
-	}
+
 
 	return (node);
 }
