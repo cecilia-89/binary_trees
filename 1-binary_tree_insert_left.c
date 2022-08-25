70415decc45554b4d2e869a1a48f0fefc4dfa034
cecilia-89 <catabong89@gmail.com>
2022-08-25 10:45:23 +0100

    corrections

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,7 +7,7 @@
  * Return: node pointer on success
  */
 
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
