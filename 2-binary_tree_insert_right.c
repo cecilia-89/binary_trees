de6f5c4a3d77d3944b6ea7a9d2a0392f056f308e
cecilia-89 <catabong89@gmail.com>
2022-08-25 10:40:05 +0100

    corrections

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,29 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - creates binary tree
- * @parent: parent of the node
- * @value: interger value
- * Return: node pointer on success
- */
-
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
-{
-	binary_tree_t *node;
-
-	node = malloc(sizeof(binary_tree_t));
-	if (node == NULL)
-		return (node);
-
-	node->left = NULL;
-	node->right = NULL;
-	node->parent = parent;
-	node->n = value;
-
-	return (node);
-}
-
-/**
  * binary_tree_insert_right - inserts node at leftmost side
  * @parent: parent of the node
  * @value: interger value
@@ -37,19 +14,19 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (0);
 
-	node = binary_tree_node(parent, value)
+	node = binary_tree_node(parent, value);
 	if (node == NULL)
 		return (0);
 
-	if (parent->left != NULL)
+	if (parent->right != NULL)
 	{
-		node->left = parent->left;
-		parent->left = node;
-		node->left->parent = node;
+		node->right = parent->right;
+		parent->right = node;
+		node->right->parent = node;
 	}
 
 	else
-		parent->left = node;
+		parent->right = node;
 
 
 	return (node);
