344952d0e3a670e5780f9c1a2f2f62971b8a6650
cecilia-89 <catabong89@gmail.com>
2022-08-26 01:36:33 +0100

    added: new tasks

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,8 +10,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	binary_tree_is_full(tree->left);
-	binary_tree_is_full(tree->right);
+	if ((tree->right == NULL) && (tree->left == NULL))
+		return (1);
 
-	return (0);
+	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+
 }
